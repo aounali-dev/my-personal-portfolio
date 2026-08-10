4edff773f7fe69aac324e52f892c866c01ce5aa6
Aoun Ali <aoundev066@gmail.com>
2026-08-10 13:11:00 -0700

    Max speed optimization: remove all heavy layout weights for mobile

--- components/SmoothScroll.jsx
+++ components/SmoothScroll.jsx
@@ -1,30 +1,10 @@
 'use client';
 
 import { useEffect } from 'react';
-import Lenis from 'lenis';
 
 export default function SmoothScroll({ children }) {
   useEffect(() => {
-    const isMobile = window.innerWidth < 768;
-
-    const lenis = new Lenis({
-      duration: isMobile ? 0.6 : 1.0, // Super snappy duration
-      easing: (t) => 1 - Math.pow(1 - t, 4), // Quicker exponential ease-out
-      smoothWheel: true,
-      touchMultiplier: isMobile ? 1.0 : 1.5,
-    });
-
-    let rafId;
-    function raf(time) {
-      lenis.raf(time);
-      rafId = requestAnimationFrame(raf);
-    }
-    rafId = requestAnimationFrame(raf);
-
-    return () => {
-      cancelAnimationFrame(rafId);
-      lenis.destroy();
-    };
+    document.documentElement.style.scrollBehavior = 'auto';
   }, []);
 
   return <>{children}</>;
